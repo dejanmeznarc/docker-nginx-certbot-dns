22b7e5f083c5b4e57282ba06e8b98dc01f72819f
dejan <dejan@meznarc.si>
2022-06-11 18:09:13 +0200

    dejansko dela

--- build/renew.sh
+++ build/renew.sh
@@ -5,7 +5,7 @@ CDATE=`date +"%Y-%m-%d %T"`
 
 echo " "
 echo " "
-echo ${CDATE} trying to renew ${DOMAIN}
+echo ${CDATE} trying to renew *.${DOMAIN}
 
 
 echo "dns_cloudflare_api_token = ${CLOUDFLARE_API_KEY}" > /cf.key.ini
@@ -15,7 +15,7 @@ certbot certonly \
   --email "${CERTBOT_EMAIL}" \
   --dns-cloudflare-credentials /cf.key.ini \
   --dns-cloudflare-propagation-seconds "${WAIT_SECONDS:-10}" \
-  -d "${DOMAIN}" \
+  -d "*.${DOMAIN}" \
   --deploy-hook "nginx -t && nginx -s reload"
 
 #nginx -t && nginx -s reload
